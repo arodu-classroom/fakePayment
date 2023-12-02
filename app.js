c1f77656d2920da58b790b22ca8228e26af893ce
Alberto Rodriguez <arodu.dev@gmail.com>
2023-12-02 18:09:36 -0400

    fix cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const paymentsRouter = require('./routes/payments');
@@ -21,12 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-  next();
-});
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/payments', paymentsRouter);
